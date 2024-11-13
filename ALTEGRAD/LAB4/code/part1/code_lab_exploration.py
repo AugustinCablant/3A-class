48efd041db54c0ddb55536e86156b34971211458
Augustin Cablant <augustin.cablant@ensae.fr>
2024-11-13 08:00:37 +0100

    up

--- ALTEGRAD/LAB4/code/part1/code_lab_exploration.py
+++ ALTEGRAD/LAB4/code/part1/code_lab_exploration.py
@@ -6,12 +6,43 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-!conda create -n altegrad-lab4 Python=3.9
-############## Task 1
 
+############## Task 1
+# Load the graph
+path = 'ALTEGRAD/LAB4/code/datasets/CA-HepTh.txt'
+G = nx.read_edgelist(
+                    path,
+                    comments = '#',
+                    delimiter = '\t',
+                    create_using = None,
+                    nodetype = None,
+                    data = True,
+                    edgetype = None,
+                    encoding = 'utf-8'
+                    )
+print("Number of nodes:", G.number_of_nodes())
+print("Number of edges:", G.number_of_edges())
+
+# Print the number of connected components in the graph
+#  A connected component : 
+# 1) Any two nodes in the subset are connected to each other by a path 
+# 2) There exists no path between a node in the subset and a node not in the subset
 
 
 ############## Task 2
+print("Number of connected components:", nx.number_connected_components(G))
+
+if nx.is_connected(G):
+    print("The graph is connected.")
+else:
+    print("The graph is not connected.")
+    print("There are :", len(list(nx.connected_components(G))), "connected components.")
+    print("The size of the largest connected component is:", len(max(nx.connected_components(G), key = len)))
+    max_connected_component = max(nx.connected_components(G), key = len)
+    largest_subgraph = G.subgraph(max_connected_component)
+    print("Number of nodes and edges of the largest connected component are :", largest_subgraph.number_of_nodes(), "and", largest_subgraph.number_of_edges())
+    print("Fraction of nodes in the largest connected component:", largest_subgraph.number_of_nodes() / G.number_of_nodes())
+    print("Fraction of edges in the largest connected component:", largest_subgraph.number_of_edges() / G.number_of_edges())
 
 
 
--- ALTEGRAD/LAB4/code/part2/code_lab_community_detection.py
+++ ALTEGRAD/LAB4/code/part2/code_lab_community_detection.py
@@ -8,21 +8,78 @@ from scipy.sparse.linalg import eigs
 from scipy.sparse import diags, eye
 from random import randint
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
+# Load the graph
+path = 'ALTEGRAD/LAB4/code/datasets/CA-HepTh.txt'
+G = nx.read_edgelist(
+                    path,
+                    comments = '#',
+                    delimiter = '\t',
+                    create_using = None,
+                    nodetype = None,
+                    data = True,
+                    edgetype = None,
+                    encoding = 'utf-8'
+                    )
 
 
 ############## Task 3
 # Perform spectral clustering to partition graph G into k clusters
 def spectral_clustering(G, k):
+    """ Perform spectral clustering to partition graph G into k clusters 
     
+    Parameters
+    ----------
+    G = (V, E): a networkx graph
+    k: number of clusters
+    
+    Returns
+    -------
+    clustering: Dictionary keyed by node to the cluster to which the node belongs
+
+    Remark : we use the same notations as the pseudo-code in the PDF
+    
+    """
     ##################
-    # your code here #
+    # Get the adjacency matrix A and the degree matrix D
+    A = nx.adjacency_matrix(G).todense()
+    degrees = dict(G.degree())
+    degree_matrix = np.diag([degrees[node] for node in G.nodes()])
+
+    # Compute the Laplacian matrix L
+    L = np.eye(G.number_of_nodes()) - np.linalg.inv(degree_matrix) @ A
+
+    # Compute the first k eigenvectors of L
+    eigvals, eigvecs = eigs(L, k = k, which = 'SR')
+
+    # Apply KMeans to the rows of the matrix of eigenvectors
+    kmeans = KMeans(n_clusters = k, random_state = 0).fit(eigvecs.real)
+    clustering = dict(zip(G.nodes(), kmeans.labels_))
     ##################
-    
-    
-    
     return clustering
 
+k = 10
+clustering_50 = spectral_clustering(G, k)
+sample_nodes = np.random.choice(list(G.nodes()), size = 500, replace=False)
+H = G.subgraph(sample_nodes)
+node_color = [clustering_50[node] for node in sample_nodes]
+
+# Visualisation du sous-graphe avec les couleurs des clusters
+plt.figure(figsize=(10, 10))
+nx.draw(H, with_labels = True, 
+        node_color = node_color, 
+        cmap = plt.cm.rainbow, 
+        node_size = 50, 
+        font_size=10)
+unique_colors = list(set(node_color))  # Liste des couleurs uniques
+patches = [mpatches.Patch(color=plt.cm.rainbow(color / max(unique_colors)), 
+                          label=f'Cluster {int(color)}') for color in unique_colors]
+plt.legend(handles=patches, title="Clusters")
+
+plt.title("Sample of Spectral Clustering")
+plt.show()
 
 ############## Task 4
 
