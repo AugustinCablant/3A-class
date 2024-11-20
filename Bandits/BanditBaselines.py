db70b5a035767681fd9075c22aa146827c2986f6
Augustin Cablant <augustin.cablant@ensae.fr>
2024-11-20 13:13:59 +0100

    up

--- Bandits/BanditBaselines.py
+++ Bandits/BanditBaselines.py
@@ -38,7 +38,7 @@ class UniformExploration:
         self.cumRewards = np.zeros(self.nbArms)
     
     def chooseArmToPlay(self):
-        return np.random.randint(0,self.nbArms)
+        return np.random.randint(0, self.nbArms)
 
     def receiveReward(self,arm,reward):
         self.cumRewards[arm] = self.cumRewards[arm]+reward
